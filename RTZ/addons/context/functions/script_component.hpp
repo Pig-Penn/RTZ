f66b7d0d9532e6393a5a94bff99796bca41c2b52
Pig-Penn <maximmelting@gmail.com>
2026-07-17 18:50:30 -0400

    big refactor

--- RTZ/addons/context/functions/script_component.hpp
+++
@@ -1,1 +0,0 @@
-#include "\x\rtz\addons\context\script_component.hpp"
--- RTZ/addons/context/script_component.hpp
+++
@@ -1,6 +0,0 @@
-#define COMPONENT context
-#define COMPONENT_BEAUTIFIED Context
-#include "\x\rtz\addons\main\script_mod.hpp"
-// #define DEBUG_MODE_FULL
-// #define DISABLE_COMPILE_CACHE
-#include "\x\rtz\addons\main\script_macros.hpp"
